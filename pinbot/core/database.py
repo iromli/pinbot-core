3e0778aa65f0e52741ffbdbed64922f16bd7f2d7
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-11-12 14:05:34 +0700

    add database interface via dataset library

---
+++ pinbot/core/database.py
@@ -0,0 +1,23 @@
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+import dataset
+import irc3
+
+
+@irc3.plugin
+class Dataset(object):
+    def __init__(self, bot):
+        self.bot = bot
+        self._db = None
+
+    @property
+    def db(self):
+        if self._db is None:
+            self._db = dataset.connect(
+                self.bot.config.get("database_url", "sqlite://"))
+        return self._db
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         "irc3",
         "six",
+        "dataset",
     ],
     packages=[
         "pinbot",
